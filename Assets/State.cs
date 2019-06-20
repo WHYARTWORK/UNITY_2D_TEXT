77b541c8a1aa0afc1788718d31aafbf6d6f268a2
WHY_ARTWORK <whyartwork@gmail.com>
2019-06-20 11:28:40 -0700

    Initial check-in of module TEXT 102

---
+++ Assets/State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+
+
+public class State : ScriptableObject 
+{
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    //Learning about methods
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
+
+
+
